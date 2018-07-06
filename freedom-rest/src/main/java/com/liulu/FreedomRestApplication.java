f3be62ce1a50b11c875098ddfd8302ee1f18a7ad
liu.lu749 <liu.lu749@chinaredstar.com>
2018-07-06 19:12:24 +0800

    代码生成

--- freedom-rest/src/main/java/com/liulu/FreedomRestApplication.java
+++ freedom-rest/src/main/java/com/liulu/FreedomRestApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.liulu.*"})
+
 public class FreedomRestApplication {
 
 	public static void main(String[] args) {
